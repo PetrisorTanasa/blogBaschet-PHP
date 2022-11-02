7fc3dc8258a57418e90d1c3b66bb27709b57883c
PetrisorTanasa <fptanasa02@yahoo.com>
2022-11-02 23:27:02 +0200

    Primul commit

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+
+echo "Salut! Acesta este proiectul meul, o revista online despre baschet";
+echo "In acest proiect am sa dezvolt un sistem de Admin(acces peste tot) -> Redactor(poate face stiri) -> Utilizator(un cititor care poate comenta) -> Cititor(o persoana random care poate doar sa citeasca";
