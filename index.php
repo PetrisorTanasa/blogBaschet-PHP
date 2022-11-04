9574c6f32456a1119b91e0d67c42f12f1084e237
PetrisorTanasa <fptanasa02@yahoo.com>
2022-11-04 09:13:22 +0200

    Modificat obiective/ideile site ului

--- index.php
+++ index.php
@@ -2,3 +2,9 @@
 
 echo "Salut! Acesta este proiectul meul, o revista online despre baschet";
 echo "In acest proiect am sa dezvolt un sistem de Admin(acces peste tot) -> Redactor(poate face stiri) -> Utilizator(un cititor care poate comenta) -> Cititor(o persoana random care poate doar sa citeasca";
+
+
+echo "Fiecare articol o sa aibe cate un tag pentru a putea fi cautat mai usor, iar la fiecare articol userii vor putea comenta asupra subiectului";
+echo "Totodata,articolele vor putea fi editate";
+
+echo "Deocamdata in mare nu mai am mari idei, o sa implementez mai intai aceste prime idei si o sa adaug pe parcurs si alte functionalitati";
